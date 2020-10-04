9591d4d8e882784fc96f06203b4de67507b3da05
Dhwrwm Basumatary <dhwrwmbazum@gmail.com>
2020-10-04 11:36:24 +0530

    Added components

---
+++ src/components/Days/Days.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Days = (props) => {
+  return <div></div>;
+};
+
+Days.propTypes = {};
+
+export default Days;
---
+++ src/components/Location/Location.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "Location.scss";
+import PropTypes from "prop-types";
+
+const Location = (props) => {
+  return <div className="Location">San Francisco</div>;
+};
+
+Location.propTypes = {};
+
+export default Location;
