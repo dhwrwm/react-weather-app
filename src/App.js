bddb02291862a4c1a9f070bd272d798caf9959a1
Dhwrwm Basumatary <dhwrwmbazum@gmail.com>
2020-10-05 14:05:06 +0530

    Added loading

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, Fragment } from "react";
+import React, { useEffect, Fragment, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import isEmpty from "lodash/isEmpty";
 
@@ -7,17 +7,19 @@ import Temperature from "./components/Temperature/Temperature";
 import Location from "./components/Location/Location";
 import Days from "./components/Days/Days";
 import Forecast from "./components/Forecast/Forecast";
+import Loading from "./components/Loading/Loading";
 import { getWeatherForcast } from "./redux/actions";
 import "./App.scss";
 
 function App() {
   const dispatch = useDispatch();
-  const { data } = useSelector((state) => state);
+  const [isLocationEnabled, setLocationEnabled] = useState(false);
+  const { data, isFetching } = useSelector((state) => state);
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(function (position) {
       const { latitude, longitude } = position.coords;
-
+      setLocationEnabled(true);
       dispatch(getWeatherForcast(latitude, longitude));
     });
   }, []);
@@ -25,9 +27,16 @@ function App() {
   return (
     <div className="App flex">
       <div className="App__wrapper flex">
-        <Location />
+        {isLocationEnabled && isEmpty(data) && isFetching ? (
+          <Loading />
+        ) : (
+          <div className="App__enable-location">
+            Please enable access to you current location.
+          </div>
+        )}
         {!isEmpty(data) && (
           <Fragment>
+            <Location />
             <Heading />
             <Temperature />
             <div className="App__main">
---
+++ src/components/Loading/Loading.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./Loading.scss";
+
+const Loading = () => {
+  return (
+    <div className="loading">
+      <div className="circle"></div>
+      <div className="circle"></div>
+      <div className="circle"></div>
+      <div className="circle"></div>
+    </div>
+  );
+};
+
+export default Loading;
